67d8c8dd9a59b1cd5bb9448c2a651f6ad8feeab8
Juliana <juliana@itsencrypted.com>
2020-01-05 22:30:53 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("I am Poor"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(child: Image(image: AssetImage('images/poor_guy.jpg'))),
+    )));
